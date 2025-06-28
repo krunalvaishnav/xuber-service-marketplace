b7c1228454180002352b57d63657d0f422952451
krunalvaishnav <krunalvaishnav2004@gmail.com>
2025-06-28 14:10:08 +0530

    add

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,24 +1,18 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-<<<<<<< HEAD
-const url = `mysql://root:WLgXDcHIWzhaQNGYlOSOlvnwRjLsSDAA@mysql.railway.internal:3306/railway`
+const url = `mysql://root:WwvWQprHrecVkCSBbEXBBiLULkMeUayv@interchange.proxy.rlwy.net:26515/railway`
 const pool = mysql.createPool(url);
-
-=======
-// const url = `mysql://root:WLgXDcHIWzhaQNGYlOSOlvnwRjLsSDAA@mysql.railway.internal:3306/railway`
-// const pool = mysql.createPool(url);
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  timezone: "Z",
-});
->>>>>>> 14cb5ef5fbd4d2759736a4d8724fc23d4de96f89
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   timezone: "Z",
+// });
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error("Error connecting to the database:", err.message);
+    console.error("Error connecting to the database:",err, err.message);
   } else {
     console.log("Connected to the SQL database");
     connection.release();
